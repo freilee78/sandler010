764c8adf8707ae3abeaef4b8da27b4aca75f8627
freilee78 <45350051+freilee78@users.noreply.github.com>
2018-12-10 22:48:14 +0100

    Update thermostat.py
    Initial upload

--- thermostat.py
+++ thermostat.py
@@ -1,1 +1,41 @@
-Test
+import os
+import glob
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+ 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+	
+while True:
+        deg_c, deg_f = read_temp()
+	print(deg_c)
+	if deg_c > 27:
+            GPIO.output(17, 1)
+        if deg_c < 25:
+            GPIO.output(17, 0)
+	time.sleep(1)
